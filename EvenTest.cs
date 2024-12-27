6975fa87bf874b128df6c8ed6f441e70e105f1fc
henrickyL <henrickyl1@gmail.com>
2024-12-27 20:45:16 -0300

    add initial test method

--- EvenTest.cs
+++ EvenTest.cs
@@ -5,7 +5,10 @@ public static class EvenTest{
     public static bool IsEven2(int x){
         return (x & 1) == 0;
     }
-    public static double Test(){
-        return 0;
+    public static double Test(int x, Func<int,bool> isEven){
+        DateTime start = DateTime.Now;
+        isEven(x);
+        DateTime end = DateTime.Now;
+        return (end - start).TotalNanoseconds;
     }
 }
