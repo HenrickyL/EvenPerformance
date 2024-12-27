3d1a64bbd706b261a8ac3143eb5f9782f6e7d554
henrickyL <henrickyl1@gmail.com>
2024-12-27 20:38:42 -0300

    add 2 methods to compare

--- EvenTest.cs
+++ EvenTest.cs
@@ -1,5 +1,10 @@
 public static class EvenTest{
-
+    public static bool IsEven(int x){
+        return (x % 2) ==0;
+    }
+    public static bool IsEven2(int x){
+        return (x & 1) == 0;
+    }
     public static double Test(){
         return 0;
     }
