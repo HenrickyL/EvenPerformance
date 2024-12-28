984433f2a2bb5a40c493223abc50871ce78d6ab4
henrickyL <henrickyl1@gmail.com>
2024-12-27 21:27:02 -0300

    Test with avarage method

--- EvenTest.cs
+++ EvenTest.cs
@@ -1,14 +1,19 @@
 public static class EvenTest{
-    public static bool IsEven(int x){
+    public static bool IsEven(uint x){
         return (x % 2) ==0;
     }
-    public static bool IsEven2(int x){
+    public static bool IsEven2(uint x){
         return (x & 1) == 0;
     }
-    public static double Test(int x, Func<int,bool> isEven){
-        DateTime start = DateTime.Now;
-        isEven(x);
-        DateTime end = DateTime.Now;
-        return (end - start).TotalNanoseconds;
+    public static double Test(List<uint> tests, Func<uint,bool> isEven){
+        double sum = 0;
+        foreach(uint test in tests){
+            DateTime start = DateTime.Now;
+                isEven(test);
+            DateTime end = DateTime.Now;
+            double time = (end - start).TotalNanoseconds;
+            sum += time;
+        }
+        return sum / tests.Count;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
 ﻿public class Program{
+
+    private static List<uint> GetRandonList(int size){
+        List<uint> list = new();
+        Random random = new Random();
+        for(int i = 0; i < size; i++){
+            list.Add((uint)random.Next());
+        }
+        return list;
+    }
+
     public static void Main(){
+        List<uint> tests = GetRandonList(1000);
+
+
+        double result1 = EvenTest.Test(tests, EvenTest.IsEven);
+        double result2 = EvenTest.Test(tests, EvenTest.IsEven2);
+        
+        Console.WriteLine($"Result to {1000} in nanoseconds");
+        Console.WriteLine($"Test 1\t\tTest 2");
+        Console.WriteLine($"{result1}\t\t{result2}");
     }
 }
